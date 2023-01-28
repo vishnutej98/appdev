34b31077377e0512d376aa01fa12792acc122906
vishnutej98 <vishnutej2016@gmail.com>
2023-01-28 10:56:20 +0530

    Code changes

--- app.py
+++ app.py
@@ -6,48 +6,18 @@
 import getpass
 
 #Dictionary for storing the user the details
-USERDETAILS = {'Vishnu':'1998', 'tej':'123'}
-
-class details():
-  def get_username():
-    username = input('Username: ').lower()
-    return username
-  
-
-  def get_password():
-    password = getpass.getpass()
-    return password
-
-
-  def verification(verify_name, name, password):
-    if verify_name.lower() == name.lower():
-      print("User found in data base...")
-      verify_key = USERDETAILS[verify_name]
-      if verify_key == password:
-        print('Login successfull')
-        return (verify_name, verify_key)
-      else:
-        print("Username and Password doesn't match\nRe-enter the credentials")
-        main()
-    else:
-      # print("Username and Password doesn't match\nRe-enter the credentials")
-      # main()
-      ...
-
+USERDETAILS = {'Vishnu':'1998', 'Tej':'123'}
 
 def main():
-  name = details.get_username()
-  password = details.get_password()
-  for verify_name in USERDETAILS:
-    details.verification(verify_name, name, password)
+  username = input("Username: ").title()
+  password = getpass.getpass("Password: ")
+  for i in USERDETAILS.keys():
+    if username == i:
+      while password != USERDETAILS.get(i):
+        password = getpass.getpass("Enter the correct password: ")
 
-  
-  print('Press Q or q button to exit')
-  if input('').lower() == 'q':
-    #Do nothing
-    exit
-  else:
-    main()
+      break
+  print("Verified!")
   
 
 if __name__ == '__main__':
